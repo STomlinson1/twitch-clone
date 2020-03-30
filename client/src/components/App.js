2f28029893dc312bba0c1b43dedcc2073f5a7810
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-03-30 16:20:47 -0400

    project start

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamList from './stream/StreamList';
+import StreamCreate from './stream/StreamCreate';
+import StreamEdit from './stream/StreamEdit';
+import StreamDelete from './stream/StreamDelete';
+import StreamShow from './stream/StreamShow';
+
+export default function App() {
+	return (
+		<div>
+			<BrowserRouter>
+				<Route path="/" exact component={StreamList} />
+				<Route path="streams/new" exact component={StreamCreate} />
+				<Route path="streams/edit" exact component={StreamEdit} />
+				<Route path="streams/delete" exact component={StreamDelete} />
+				<Route path="streams/show" exact componetn={StreamShow} />
+			</BrowserRouter>
+		</div>
+	);
+}
